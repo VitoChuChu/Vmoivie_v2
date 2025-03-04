ecfea5f9316aa7e8752a56239eba34f34a15092f
VitoChuChu <vitochu1220@gmail.com>
2025-03-04 16:14:12 +0800

    db and backend deploy successed

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,12 +3,6 @@ import { DataSource } from "typeorm";
 import { User } from "./models/User";
 import { Wishlist } from "./models/Wishlist";
 
-console.log(process.env.DB_PORT);
-console.log(process.env.DB_USERNAME);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_NAME);
-console.log(process.env.DB_HOST);
-
 export const AppDataSource = new DataSource({
   type: "postgres",
   port: Number(process.env.DB_PORT),
