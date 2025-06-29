7afe95fdf9428ac864d95dd45f552554045c57c8
Vito Chu <107838632+VitoChuChu@users.noreply.github.com>
2025-06-29 19:23:46 +0800

    Style search page like login and fix footer

--- frontend/src/container/Search/Search.tsx
+++ frontend/src/container/Search/Search.tsx
@@ -14,6 +14,12 @@ const Search: React.FC<SearchProps> = ({ scrollToTop }) => {
   const [searchKey, setSearchKey] = useState<string>("");
   const [movies, setMovies] = useState<Movie[]>([]);
 
+  const containerStyle = {
+    boxShadow: "0 0 10px 5px rgba(216, 216, 216, 0.3)",
+    padding: "1.5rem",
+    borderRadius: "16px",
+  };
+
   const handleSearch = async (value: string) => {
     if (!value) return;
     const data = await fetchSearchMovies(value);
@@ -22,15 +28,20 @@ const Search: React.FC<SearchProps> = ({ scrollToTop }) => {
 
   return (
     <CenterCenterRow>
-      <CenterCenterCol span={24} style={{ marginTop: "8vh" }}>
-        <Input.Search
-          placeholder="Search movies"
-          enterButton
-          value={searchKey}
-          onChange={(e) => setSearchKey(e.target.value)}
-          onSearch={handleSearch}
-          style={{ maxWidth: 300 }}
-        />
+      <CenterCenterCol span={24} style={{ height: "20vh" }}></CenterCenterCol>
+      <CenterCenterCol span={24}>
+        <StyledH1>Search</StyledH1>
+      </CenterCenterCol>
+      <CenterCenterCol xs={16} sm={12} md={10} lg={8}>
+        <div style={containerStyle}>
+          <Input.Search
+            placeholder="Search movies"
+            enterButton
+            value={searchKey}
+            onChange={(e) => setSearchKey(e.target.value)}
+            onSearch={handleSearch}
+          />
+        </div>
       </CenterCenterCol>
       {movies.length === 0 ? (
         <CenterCenterCol span={24} style={{ marginTop: "2vh" }}>
@@ -38,7 +49,14 @@ const Search: React.FC<SearchProps> = ({ scrollToTop }) => {
         </CenterCenterCol>
       ) : (
         movies.map((item) => (
-          <CenterCenterCol xs={22} sm={11} md={8} lg={4} key={item.id} style={{ marginTop: "2vh" }}>
+          <CenterCenterCol
+            xs={22}
+            sm={11}
+            md={8}
+            lg={4}
+            key={item.id}
+            style={{ marginTop: "2vh" }}
+          >
             <MoviesCard item={item} scrollToTop={scrollToTop} />
           </CenterCenterCol>
         ))
